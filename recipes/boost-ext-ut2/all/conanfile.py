b1eb7e23343dffbe21c354d53136be9ae6ab5470
FeignClaims <www4112379@163.com>
2024-04-15 21:00:36 +0800

    [boost-ext-ut2] Init

---
+++ recipes/boost-ext-ut2/all/conanfile.py
@@ -0,0 +1,75 @@
+import os
+
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import cmake_layout
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, download
+from conan.tools.scm import Version
+
+required_conan_version = ">=2.0.0"
+
+
+class Ut2Conan(ConanFile):
+    name = "boost-ext-ut2"
+    description = "UT2: C++20 minimal/compile-time first unit-testing library"
+    license = "BSL-1.0"
+    url = "https://github.com/FeignClaims/customized_conan_recipes"
+    homepage = "https://github.com/boost-ext/ut2"
+    topics = ("testing", "compile-time", "run-time", "cpp20", "header-only")
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
+    no_copy_source = True
+
+    @property
+    def _min_cppstd(self):
+        return 20
+
+    @property
+    def _compilers_minimum_version(self):
+        return {"gcc": "12",
+                "clang": "16"}
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+            raise ConanInvalidConfiguration(
+                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+            )
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        download(self, url="https://www.boost.org/LICENSE_1_0.txt", filename="LICENSE")
+
+    def generate(self):
+        pass
+
+    def build(self):
+        apply_conandata_patches(self)
+
+    def package(self):
+        copy(self, pattern="LICENSE*",
+             src=self.source_folder,
+             dst=os.path.join(self.package_folder, "licenses"))
+        copy(self,
+             pattern="ut2",
+             src=self.source_folder,
+             dst=os.path.join(self.package_folder, "include"))
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
+        self.cpp_info.set_property("cmake_file_name", "ut2")
+        self.cpp_info.set_property("cmake_target_name", "boost-ext-ut2::ut2")
