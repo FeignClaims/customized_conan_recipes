56b542a51a38506a2f96906f681d58cabe823cb2
FeignClaims <www4112379@163.com>
2024-04-16 14:24:22 +0800

    [boost-ext-mp] Update to new version schema

--- recipes/boost-ext-mp/all/conanfile.py
+++ recipes/boost-ext-mp/all/conanfile.py
@@ -3,47 +3,39 @@ import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, CMakeToolchain
-from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy
-from conan.tools.layout import basic_layout
-from conan.tools.scm import Version, Git
+from conan.tools.cmake import cmake_layout
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy, download
+from conan.tools.scm import Version
 
 required_conan_version = ">=2.0.0"
 
 
 class MpConan(ConanFile):
     name = "boost-ext-mp"
-    description = "MP: C++20 ~~Template~~ Meta-Programming"
+    description = "MP: C++17 ~~Template~~ Meta-Programming library"
     license = "BSL-1.0"
     url = "https://github.com/FeignClaims/customized_conan_recipes"
-    homepage = "https://boost-ext.github.io/mp/"
-    topics = ("reflection", "stl", "compile-time", "meta-programming", "ranges", "cpp20", "header-only")
+    homepage = "https://github.com/boost-ext/mp"
+    topics = ("compile-time", "meta-programming", "ranges", "cpp17", "header-only")
     package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
-    options = {
-        "disable_module": [True, False]
-    }
-    default_options = {
-        "disable_module": False
-    }
 
     @property
     def _min_cppstd(self):
-        return 20
+        return 17
 
     @property
     def _compilers_minimum_version(self):
         return {"msvc": "193",
-                "gcc": "11",
-                "clang": "13",
-                "apple-clang": "14"}
+                "gcc": "12",
+                "clang": "13"}
 
     def export_sources(self):
         export_conandata_patches(self)
 
     def layout(self):
-        basic_layout(self, src_folder="src")
+        cmake_layout(self, src_folder="src")
 
     def package_id(self):
         self.info.clear()
@@ -57,42 +49,23 @@ class MpConan(ConanFile):
                 f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
             )
 
-    @property
-    def _should_test(self):
-        return not self.conf.get("tools.build:skip_test", default=True, check_type=bool)
-
-    def build_requirements(self):
-        if self._should_test:
-            self.test_requires("boost-ext-ut/1.1.9")
-        self.tool_requires("cmake/[>=3.23 <4.0.0]")
-
     def source(self):
-        data = self.conan_data["sources"][self.version]
-        git = Git(self)
-        git.clone(data["url"], target=".", args=["--recursive"])
-        git.checkout(data["commit"])
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        download(self, url="https://www.boost.org/LICENSE_1_0.txt", filename="LICENSE")
 
     def generate(self):
-        toolchain = CMakeToolchain(self)
-        toolchain.cache_variables["BOOST_MP_BUILD_EXAMPLES"] = False
-        toolchain.cache_variables["BOOST_MP_BUILD_TESTS"] = self._should_test
-        # TODO: cmake.install() and support module
-        toolchain.cache_variables["BOOST_MP_DISABLE_MODULE"] = self.options.get_safe("disable_module", False)
-        toolchain.generate()
+        pass
 
     def build(self):
         apply_conandata_patches(self)
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
 
     def package(self):
         copy(self, pattern="LICENSE*",
              src=self.source_folder,
              dst=os.path.join(self.package_folder, "licenses"))
         copy(self,
-             pattern="*",
-             src=os.path.join(self.source_folder, "include"),
+             pattern="mp",
+             src=self.source_folder,
              dst=os.path.join(self.package_folder, "include"))
 
     def package_info(self):
--- recipes/boost-ext-mp/all/test_package/test_package.cpp
+++ recipes/boost-ext-mp/all/test_package/test_package.cpp
@@ -1,18 +1,10 @@
-#include <cassert>
-#include <ranges>
-#include <tuple>
+#include <array>
+#include <reflect>
+#include <type_traits>
 
-#include <boost/mp.hpp>
+template <auto N, class... Ts>
+using at_c = mp::type_of<std::array{mp::meta<Ts>...}[N]>;
 
-int main(int argc, char const**) {
-  namespace mp = boost::mp;
-  using mp::operator""_c;
-
-  auto slice = [](auto list, auto begin, auto end) {
-    using mp::operator|;
-    return list | std::views::drop(begin)  // use std.ranges
-           | std::views::take(end - 1_c);  // any library which can operate on containers is supported!
-  };
-
-  assert((slice(std::tuple{1, argc, 3, 4}, 1_c, 3_c) == std::tuple{argc, 3}));
-}
+static_assert(std::is_same_v<int, at_c<0, int, bool, float>>);
+static_assert(std::is_same_v<bool, at_c<1, int, bool, float>>);
+static_assert(std::is_same_v<float, at_c<2, int, bool, float>>);
